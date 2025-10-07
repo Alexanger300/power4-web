a699fced0d73798aa8d8afdd8b0b4eb95fcdc0e2
Alexanger300 <alexandrefatoux007@gmail.com>
2025-10-07 17:44:02 +0200

    Alexandre

--- main.go
+++ main.go
@@ -4,11 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"power4-web/game"
-	"strconv"
 )
-
-var currentGame *game.Game
 
 func main() {
 	// Servir les dossiers statiques
@@ -21,37 +17,6 @@ func main() {
 		http.ServeFile(w, r, "./home_page/home_page.html")
 	})
 
-	// Démarrer une partie
-	http.HandleFunc("/start", func(w http.ResponseWriter, r *http.Request) {
-		currentGame = game.NewGame()
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		fmt.Fprint(w, currentGame.BoardText())
-	})
-
-	// Jouer un coup
-	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
-		if currentGame == nil {
-			http.Error(w, "Aucune partie en cours", http.StatusBadRequest)
-			return
-		}
-
-		colStr := r.URL.Query().Get("col")
-		col, err := strconv.Atoi(colStr)
-		if err != nil {
-			http.Error(w, "Colonne invalide", http.StatusBadRequest)
-			return
-		}
-
-		if !currentGame.DropDisc(col) {
-			http.Error(w, "Coup invalide", http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		fmt.Fprint(w, currentGame.BoardText())
-	})
-
-	// Lancer le serveur
 	port := ":8080"
 	fmt.Printf("Serveur démarré sur http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
