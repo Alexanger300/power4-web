5a147b740ccfa2d3379c30bdfbe938457e569bc3
Alexanger300 <alexandrefatoux007@gmail.com>
2025-10-08 14:46:44 +0200

    Alexandre

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"power4-web/game"
 )
 
 func main() {
-	// Servir les dossiers statiques
+	// Initialisation du jeu
+	game.InitGame(6, 7)
+	game.ServeGameHandlers()
+
+	// Servir les fichiers statiques
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 	http.Handle("/power4/", http.StripPrefix("/power4/", http.FileServer(http.Dir("./power4"))))
 	http.Handle("/home_page/", http.StripPrefix("/home_page/", http.FileServer(http.Dir("./home_page"))))
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
 
-	// Page d’accueil
+	// Page d'accueil
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./home_page/home_page.html")
 	})
 
 	port := ":8080"
-	fmt.Printf("Serveur démarré sur http://localhost%s\n", port)
+	log.Printf("Serveur démarré sur http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
