f92c5ed4352ae408827cdb3e51d5d1ddd132c805
knilaksh <nilakshan.konesalingam@ynov.com>
2025-10-30 17:04:29 +0100

    Nilakshan

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"log"
 	"net/http"
+
+	"github.com/pkg/browser"
 )
 
 func main() {
-	// Initialisation du jeu
-
-	// Servir les fichiers statiques
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
-	http.Handle("/power4/", http.StripPrefix("/power4/", http.FileServer(http.Dir("./power4"))))
-	http.Handle("/home_page/", http.StripPrefix("/home_page/", http.FileServer(http.Dir("./home_page"))))
-	http.Handle("/login/", http.StripPrefix("/login/", http.FileServer(http.Dir("./login"))))
-
-	// Page d'accueil
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./login/inscription.php")
-	})
-
-	port := ":8080"
-	log.Printf("Serveur démarré sur http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	http.Handle("/", http.FileServer(http.Dir("./")))
+	go browser.OpenURL("http://localhost/power4-web-1/home_page/home_page.html") // ouvre le navigateur
+	http.ListenAndServe(":8080", nil)
 }
